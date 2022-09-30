612dcc2bdd469cf382501009e1bc4d0a71ccdde6
kai-equinix <kafan@ap.equinix.com>
2022-09-30 10:55:11 +0800

    fixed bugs

--- assignment/src/main/java/com/Datwyler/CreditFacility/assignment/repositories/LoanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Datwyler.CreditFacility.assignment.repositories;
-
-import com.Datwyler.CreditFacility.assignment.models.Loan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanRepository extends JpaRepository<Loan,Long> {
-	
-}
